748db3f38ffb89a2ee56c18b03baaa4c75894d1b
Morten Empeño <morten@empeno.dk>
2020-12-07 08:21:17 +0100

    initial function for adding employees to queue

--- api/AddEmployee.cs
+++ api/AddEmployee.cs
@@ -7,6 +7,12 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace EmpenosSmartLearningStaticWebApp
 {
@@ -14,9 +20,33 @@ namespace EmpenosSmartLearningStaticWebApp
     {
         [FunctionName("AddEmployee")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ExecutionContext executionContext,
             ILogger log)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(executionContext.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+
+            string connectionString = config["StorageAccountConnectionString"];
+
+
+
+            // Create a unique name for the queue
+            string queueName = "employeesqueue";
+
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+
+            await queueClient.CreateAsync();
+
+            await queueClient.SendMessageAsync("First message");
+            await queueClient.SendMessageAsync("Second message");
+
+            
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
